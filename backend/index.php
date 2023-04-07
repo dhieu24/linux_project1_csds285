d286a9aaab0dd6d1f1f9ffc8e06eff493c9a5037
hieudangk27 <hieudangk27@gmail.com>
2023-04-07 00:29:47 -0400

    Add comments for JS and PHP

--- backend/index.php
+++ backend/index.php
@@ -1,6 +1,7 @@
 <?php 
     $json = file_get_contents('php://input');
-    // echo $json;
+    
+    // decode the JSON data from front-end
     $data = json_decode($json);
 
     $q = $data->q;
@@ -10,6 +11,8 @@
     $language = $data->language;
     $domain = $data->domain;
 
+
+    // create a map of keys and values for validation
     $arr = array(
         'q' => $q,
         'qInTitle' => $qInTitle,
@@ -19,23 +22,21 @@
         'domain' => $domain
     );
     
+    // fixed API key from Newsdata.io API provider. This field should come from the user if the application is scaled up
     $apiKey = "pub_19907fa17383b4f8fc3affc20b1e91390713b";
+    // prepare the url for sending data
     $url = "https://newsdata.io/api/1/news?apikey=" . $apiKey;
+    // loop over the map to validate each keys and values
     foreach ($arr as $key => $value) {
         if (!empty($value)) {
           $url .= "&" . $key . "=" . urlencode($value);
         }
     }
 
-    // echo $url;
+    // retrieve the response from the external API
     $response = file_get_contents($url);
 
-
-    // header('Content-Type: application/json');
+    // convert the data into JSON format and return to the front-end for UI rendering
     $return_data = json_encode($response);
     echo $return_data;
-
-
-    // header('Location: .././result.html', json_encode($response));
-    // echo $response;
 ?>
